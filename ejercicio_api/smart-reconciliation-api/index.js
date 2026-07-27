20aefb888a780143b41346dbcb1c80dd9f36682c
Javier Martinez <javier.martinezperches01@gmail.com>
2026-07-26 18:42:46 -0600

    hicimos los apis de get, post users

--- ejercicio_api/smart-reconciliation-api/index.js
+++ ejercicio_api/smart-reconciliation-api/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const users = [];
+
 app.get("/", (req, res) => {
     res.json({
         message: "API funcionando"
     });
+});
+
+app.post("/users", (req, res) => {
+    console.log(req.body);   
+    users.push(req.body);
+    res.json({
+        message: "Usuario guardado",
+        data: req.body
+    });
+});
+
+app.get("/users", (req, res) => {
+    res.json(users);
 });
 
 app.listen(3000, () => {
